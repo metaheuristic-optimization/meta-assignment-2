2fb8e71cb47d276cdd3d75a14258f95373e4cd12
el-davo <david.ahrn26@gmail.com>
2018-12-16 18:52:28 +0000

    Reading data from data files

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 from src import Utils
 
+utils = Utils()
+
+cnf = utils.load_dimacs_cnf_file('./datasets/uf20-020.cnf')
+
+print(cnf)
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,34 @@
 class Utils:
 
-    def loadDataset(self):
-        pass
+    def load_dimacs_cnf_file(self, cnf_file):
+        """
+        Reads and parses Dimacs CNF formatted files. Note much of this code was taken from stack-overflow
+
+        https://stackoverflow.com/questions/28890268/parse-dimacs-cnf-file-python
+
+        :param cnf_file: The path to the cnf file
+        :return: Parsed cnf file
+        """
+        with open(cnf_file) as file:
+            content = file.readlines()
+
+        cnf = list()
+        cnf.append(list())
+        max_var = 0
+
+        for line in content:
+            tokens = line.split()
+            if len(tokens) != 0 and tokens[0] not in ("p", "c", "%"):
+                for token in tokens:
+                    lit = int(token)
+                    max_var = max(max_var, abs(lit))
+                    if lit == 0:
+                        cnf.append(list())
+                    else:
+                        cnf[-1].append(lit)
+
+        assert len(cnf[-1]) == 0
+
+        cnf.pop()
+
+        return cnf, max_var
