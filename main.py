91adc7baabefc21b09a7a469daf32894eb529e7b
el-davo <david.ahrn26@gmail.com>
2018-12-17 18:48:32 +0000

    Added test file

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from src import Utils
 
 utils = Utils()
 
-cnf, max_var = utils.load_dimacs_cnf_file('./datasets/uf20-020.cnf')
+max_var, cnf = utils.load_dimacs_cnf_file('./datasets/test.cnf')
 
 print(cnf)
-print(max_var)
