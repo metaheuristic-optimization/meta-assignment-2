23f955669442751ba3e6d6f1d03c3195525fe674
el-davo <david.ahrn26@gmail.com>
2018-12-18 22:42:56 +0000

    Multi process

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from src import Utils
 from src import GSAT
+from multiprocessing import Process
 
-utils = Utils()
-cnf = utils.load_dimacs_cnf_file('./datasets/uf20-020.cnf')
-
-for i in range(10):
+def run():
+    utils = Utils()
+    cnf = utils.load_dimacs_cnf_file('./datasets/uf20-020.cnf')
     gsat = GSAT(formula = cnf, max_steps = 100, max_iterations = 1000, tabu_max_length = 5)
     solution = gsat.run()
 
     print(solution)
+
+
+if __name__ == '__main__':
+    for i in range(100):
+        p = Process(target=run)
+        p.start()
