764137769f7d09c63bee73916db375718f140b40
el-davo <david.ahrn26@gmail.com>
2018-12-22 21:54:20 +0000

    Updated

--- main_tsp_nn.py
+++ main_tsp_nn.py
@@ -1,5 +1,5 @@
 from src.tsp import TSP
 
-tsp = TSP('./datasets/tsp/inst-0.tsp', total_iterations=5, local_search_time_limit=60, starting_algorithm='nearest_neighbours')
+tsp = TSP('./datasets/tsp/inst-0.tsp', total_iterations=5, local_search_time_limit=300, starting_algorithm='nearest_neighbours')
 
 tsp.run()
--- main_tsp_rt.py
+++ main_tsp_rt.py
@@ -1,5 +1,5 @@
 from src.tsp import TSP
 
-tsp = TSP('./datasets/tsp/inst-0.tsp', total_iterations=5, local_search_time_limit=60, starting_algorithm='random_tours')
+tsp = TSP('./datasets/tsp/inst-0.tsp', total_iterations=5, local_search_time_limit=300, starting_algorithm='random_tours')
 
 tsp.run()
