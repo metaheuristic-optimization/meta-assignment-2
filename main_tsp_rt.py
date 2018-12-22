028541ab6ca499f98600035d971803388cc5b495
el-davo <david.ahrn26@gmail.com>
2018-12-22 21:58:21 +0000

    Updated

--- main_tsp_rt.py
+++ main_tsp_rt.py
@@ -1,5 +1,6 @@
 from src.tsp import TSP
+import sys
 
-tsp = TSP('./datasets/tsp/inst-0.tsp', total_iterations=5, local_search_time_limit=300, starting_algorithm='random_tours')
+tsp = TSP('./datasets/tsp/' + sys.argv[1], total_iterations=5, local_search_time_limit=300, starting_algorithm='random_tours')
 
 tsp.run()
