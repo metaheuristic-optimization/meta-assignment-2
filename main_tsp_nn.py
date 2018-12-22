1bd62ea7eb8f12a49db4a7fbcedb6d4fa9506a78
el-davo <david.ahrn26@gmail.com>
2018-12-22 22:00:19 +0000

    Adding experiments

--- main_tsp_nn.py
+++ main_tsp_nn.py
@@ -1,6 +1,9 @@
 from src.tsp import TSP
 import sys
 
-tsp = TSP('./datasets/tsp/' + sys.argv[1], total_iterations=5, local_search_time_limit=300, starting_algorithm='nearest_neighbours')
+for i in range(5):
+    print('=======Experiment {0}======='.format(i))
 
-tsp.run()
+    tsp = TSP('./datasets/tsp/' + sys.argv[1], total_iterations=5, local_search_time_limit=300, starting_algorithm='nearest_neighbours')
+
+    tsp.run()
