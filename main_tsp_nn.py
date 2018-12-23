fa19b50d0ee6f374cf4cc7e4c06c4008540d3ea5
el-davo <david.ahrn26@gmail.com>
2018-12-23 18:06:09 +0000

    More comments

--- main_tsp_nn.py
+++ main_tsp_nn.py
@@ -1,9 +1,14 @@
 from src.tsp import TSP
 import sys
 
+"""
+Entry point for TSP with Nearest Neighbours
+"""
 for i in range(5):
+    # Print the experiment number
     print('=======Experiment {0}======='.format(i))
 
+    # Create and run a new TSP instance with values
     tsp = TSP('./datasets/tsp/' + sys.argv[1], total_iterations=5, local_search_time_limit=300, starting_algorithm='nearest_neighbours')
 
     tsp.run()
