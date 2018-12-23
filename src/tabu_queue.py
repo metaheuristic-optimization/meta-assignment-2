a882b91fb4266ca4f9cdfd0adbf49103970e7c8d
el-davo <david.ahern@emc.com>
2018-12-23 10:51:32 +0000

    Added comments to tabu queue

--- src/tabu_queue.py
+++ src/tabu_queue.py
@@ -1,20 +1,36 @@
+"""
+Simple implementation of a tabu queue using a list
+"""
+
+
 class Tabu:
     queue = []
     max_length = 5
 
     def __init__(self, max_length):
+        # Determine max length
         self.max_length = max_length
 
     def reset(self):
+        """
+        Resets the queue to be empty
+        """
         self.queue = []
 
     def add_to_queue(self, item):
+        """
+        Append an item to the queue
+        :param item: An item to add to the queue
+        """
         self.queue.append(item)
 
+        # Make sure we have filled the queue up before removing items from the queue
         if len(self.queue) == self.max_length:
             self.queue.pop(0)
 
     def is_item_in_queue(self, item):
-        # print('checking item in queue {0}'.format(item))
-        # print(self.queue)
+        """
+        Checks if an item is in the tabu list
+        :param item: True if the item is in the queue, otherwise False
+        """
         return item in self.queue
